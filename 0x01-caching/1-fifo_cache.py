591be99de0132da8f3135267d3c99e09ac09a4fe
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-04-09 13:40:02 +0300

    Implemented FIFOCache class with FIFO caching system and item discarding

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+from .base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    FIFOCache class. Inherits from BaseCaching. Uses FIFO caching system.
+    """
+    def __init__(self):
+        """
+        Initialize
+        """
+        super().__init__()
+        self.keys = []
+
+    def put(self, key, item):
+        """
+        Add an item in the cache
+        """
+        if key is None or item is None:
+            return
+        if key in self.cache_data:
+            self.keys.remove(key)
+        elif len(self.cache_data) >= self.MAX_ITEMS:
+            discarded_key = self.keys.pop(0)
+            del self.cache_data[discarded_key]
+            print(f"DISCARD: {discarded_key}")
+        self.cache_data[key] = item
+        self.keys.append(key)
+
+    def get(self, key):
+        """
+        Get an item by key
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
