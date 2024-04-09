b93202f2b8407e5517e2795b636650d7a9460b9d
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-04-09 13:07:26 +0300

    Implemented BasicCache class inheriting from BaseCaching with unlimited storage

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+from .base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    BasicCache class. Inherits from BaseCaching. Has no limit.
+
+    Methods:
+        put(key, item): Add an item in the cache.
+        get(key): Get an item by key.
+    """
+    def put(self, key, item):
+        """
+        Add an item in the cache
+
+        Args:
+            key: The key of the item.
+            item: The item to be added.
+
+        Returns:
+            None
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Get an item by key
+
+        Args:
+            key: The key of the item.
+
+        Returns:
+            The item associated with the key, or None if the key is not found.
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
