c1294a96dfeb14043d614d124dbdb1c48b5d661b
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-04-20 13:10:04 +0300

    Create basic index.html template with title and header

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -4,20 +4,8 @@ This module defines a simple Flask app
 """
 
 from flask import Flask, render_template
-from flask_babel import Babel
-
-
-class Config(object):
-    """
-    Configuration class for Babel
-    """
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 app = Flask(__name__)
-app.config.from_object(Config)
-babel = Babel(app)
 
 
 @app.route('/')
@@ -26,7 +14,7 @@ def home():
     Define the route for the home page("/")
 
     Returns:
-    A rendered HTML template as a response
+    A rendered HTML template as a response.
     """
     return render_template('0-index.html')
 
