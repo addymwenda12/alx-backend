4b7c0e17fdc05749c37cf44e59bf0ec36d2f33dc
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-04-20 13:34:17 +0300

    Implement internationalization in templates using Flask-Babel

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""
+This module defines a simple Flask app
+"""
+
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+
+class Config(object):
+    """
+    Configuration class for Babel
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    Determine the best match with our supported languages
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def home():
+    """
+    Define the route for the home page("/")
+
+    Returns:
+    A rendered HTML template as a response
+    """
+    return render_template('2-index.html', title=_("home_title"), header=_("home_header"))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
