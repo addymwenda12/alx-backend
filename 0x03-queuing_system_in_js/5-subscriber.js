d2599ef07be5d69778b9f1085342031d719aa5de
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-05-07 18:01:59 +0300

    Added Redis publisher and subscriber scripts

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,22 @@
+import redis from 'redis';
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('connect', function() {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', function(err) {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+client.subscribe('holberton school channel');
+
+client.on('message', (channel, message) => {
+  console.log(message);
+  if (message === 'KILL_SERVER') {
+    client.unsubscribe();
+    client.quit();
+  }
+});
