d1dffb8024f68d72ce749063c467d068d3e6b092
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-05-08 15:06:50 +0300

    Implemented job processing with Kue for push notifications

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -3,8 +3,8 @@ import kue from 'kue';
 const queue = kue.createQueue();
 
 const jobData = {
-  phoneNumber: '1234567890',
-	message: 'This is the message',
+  phoneNumber: '4153518780',
+	message: 'This is the code to verify your account',
 };
 
 const job = queue.create('push_notification_code', jobData)
@@ -20,4 +20,3 @@ job.on('complete', () => {
 job.on('failed', () => {
 	console.log('Notification job failed');
 });
-	
